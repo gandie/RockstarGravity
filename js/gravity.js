2a8642258805d1f19b3e825feceaf992f63b9007
Lars Bergmann <lars@bergmann82.de>
2024-09-29 17:26:01 +0200

    move main loop into separate script

--- js/gravity.js
+++ js/gravity.js
@@ -1,55 +1,7 @@
 var gravity_rockstar = `${rockstar_constants}
 ${rockstar_sqrt}
 ${newton_force}
-Listen to the universe
-Shatter the universe into atoms with USA
-Put atoms into atomslen
-Rock the ether
-Put Stephen Hawking into outerloopindex
-While outerloopindex is weaker than atomslen
-Let curatom be atoms at outerloopindex
-Shatter curatom into curquarks with Germany
-Let curx be curquarks at Stephen Hawking
-Let cury be curquarks at Marie Curie
-Let curvx be curquarks at Carl Sagan
-Let curvy be curquarks at Edwin Hubble
-Let curm be curquarks at Albert Einstein
-Cast curx with 10
-Cast cury with 10
-Cast curvx with 10
-Cast curvy with 10
-Cast curm with 10
-Put Stephen Hawking into curaccx
-Put Stephen Hawking into curaccy
-Put Stephen Hawking into innerloopindex
-While innerloopindex is weaker than atomslen
-Let inneratom be atoms at innerloopindex
-if curatom ain't inneratom
-Put Newton taking curatom, and inneratom into the answer
-Let resforcex be the answer at Stephen Hawking
-Let resforcey be the answer at Marie Curie
-Put resforcex over curm into resforcex
-Put resforcey over curm into resforcey
-Put curaccx with resforcex into curaccx
-Put curaccy with resforcey into curaccy
-
-Build innerloopindex up
-
-Put curvx with curaccx into curvx
-Put curvy with curaccy into curvy
-Put curx with curvx into curx
-Put cury with curvy into cury
-Let newatom at Stephen Hawking be curx
-Let newatom at Marie Curie be cury
-Let newatom at Carl Sagan be curvx
-Let newatom at Edwin Hubble be curvy
-Let newatom at Albert Einstein be curm
-Unite newatom into resultatom with Germany
-Rock the ether with resultatom
-Build outerloopindex up
-
-Unite the ether into resultuniverse with USA
-Give resultuniverse back
+${gravity_main}
 `
 
 /*
